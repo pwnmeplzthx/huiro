c14d1ad91de006c8679be271f4c56d58315d6cf8
pwnmeplzthx <pshumakov11@mail.ru>
2025-12-23 01:21:18 +0300

    настройка eslinta для группировки импортов

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { eslintBoundariesConfig } from "./eslint.boundaries.js";
+import { eslintBoundariesConfig } from "./eslint.boundaries.js"
+import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -17,6 +18,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'import': importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,6 +27,58 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'import': importPlugin,
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          'groups': [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+            'type'
+          ],
+          'pathGroups': [
+            {
+              'pattern': '@/**',
+              'group': 'internal',
+              'position': 'after'
+            }
+          ],
+          'pathGroupsExcludedImportTypes': ['builtin'],
+          'newlines-between': 'always',
+          'alphabetize': {
+            'order': 'asc',
+            'caseInsensitive': true
+          },
+          'warnOnUnassignedImports': false
+        }
+      ],
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+    },
+    settings: {
+      'import/resolver': {
+        'typescript': {
+          'project': './tsconfig.json'
+        },
+        'node': {
+          'extensions': ['.js', '.jsx', '.ts', '.tsx']
+        }
+      },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx']
+      }
+    }
   },
   eslintBoundariesConfig
 )
